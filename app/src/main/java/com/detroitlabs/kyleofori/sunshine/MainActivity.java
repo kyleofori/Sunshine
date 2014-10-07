4c6fda984fec862d304f555e864a887e7062bf7e
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-10-07 17:59:50 -0400

    Making an ArrayAdapter

--- app/src/main/java/com/detroitlabs/kyleofori/sunshine/MainActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/sunshine/MainActivity.java
@@ -11,7 +11,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -59,6 +62,21 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            ArrayList<String> weatherStrings = new ArrayList<String>();
+            String weather109 = "Thu - sunny, 56 / 43";
+            String weather110 = "Fri - sunny, 96 / 22";
+            weatherStrings.add(getString(R.string.weather_107)); //You have to getString() for string resources
+            weatherStrings.add(getString(R.string.weather_108));
+            weatherStrings.add(weather109);
+            weatherStrings.add(weather110);
+
+            ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<String>(
+                    getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    weatherStrings);
+            
             return rootView;
         }
     }
