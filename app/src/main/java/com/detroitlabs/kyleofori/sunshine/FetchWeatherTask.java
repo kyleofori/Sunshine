042bd7634c2a6e9e4aa775b7d6f00945510aae81
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-10-15 17:13:14 -0400

    Copying and pasting getWeatherDataFromJson method into new WeatherReport class, and instantiating a WeatherReport in the FetchWeatherTask class

--- app/src/main/java/com/detroitlabs/kyleofori/sunshine/FetchWeatherTask.java
+++ app/src/main/java/com/detroitlabs/kyleofori/sunshine/FetchWeatherTask.java
@@ -90,66 +90,66 @@ public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
      * pull out the data we need to construct the Strings needed for the wireframes.
      */
 
-    private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
-            throws JSONException {
+    WeatherReport myWeatherReport = new WeatherReport();
 
-        // These are the names of the JSON objects that need to be extracted.
-        final String OWM_LIST = "list";
-        final String OWM_WEATHER = "weather";
-        final String OWM_TEMPERATURE = "temp";
-        final String OWM_MAX = "max";
-        final String OWM_MIN = "min";
-        final String OWM_DATETIME = "dt";
-        final String OWM_DESCRIPTION = "main";
-
-        ArrayList<String> resultsArrayList = new ArrayList<String>();
-
-        JSONObject forecastJson = new JSONObject(forecastJsonStr); //this one is not in the JSON data.
-        JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST); //this is found in the list object of the JSON data.
-
-        String[] resultStrs = new String[numDays];
-        for (int i = 0; i < weatherArray.length(); i++) {
-            // For now, using the format "Day, description, hi/low"
-            String day;
-            String description;
-            String highAndLow;
-
-            // Get the JSON object representing the day
-            JSONObject dayForecast = weatherArray.getJSONObject(i);
-
-            // The date/time is returned as a long.  We need to convert that
-            // into something human-readable, since most people won't read "1400356800" as
-            // "this saturday".
-            long dateTime = dayForecast.getLong(OWM_DATETIME);
-            day = getReadableDateString(dateTime);
-
-            // description is in a child array called "weather", which is 1 element long.
-            JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
-            description = weatherObject.getString(OWM_DESCRIPTION);
-
-            // Temperatures are in a child object called "temp".  Try not to name variables
-            // "temp" when working with temperature.  It confuses everybody.
-            JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
-            double high = temperatureObject.getDouble(OWM_MAX);
-            double low = temperatureObject.getDouble(OWM_MIN);
-
-            // KO - I'd like a check for the setting that we're on, which would multiply
-            // the Celsius temperature by 1.8 and add 32 if the mode were imperial.
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
-
-            boolean isImperial = prefs.getString("temperature",     //prefs.getString() has 2 parameters
-                    mContext.getString(R.string.pref_temp_label)).equals("Imperial");
-            if (isImperial) {
-                high = convertToFahrenheit(high);
-                low = convertToFahrenheit(low);
-            }
-
-            highAndLow = formatHighLows(high, low);
-            resultStrs[i] = day + " - " + description + " - " + highAndLow;
-        }
-
-        return resultStrs;
-    }
+//    private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
+//            throws JSONException {
+//
+//        // These are the names of the JSON objects that need to be extracted.
+//        final String OWM_LIST = "list";
+//        final String OWM_WEATHER = "weather";
+//        final String OWM_TEMPERATURE = "temp";
+//        final String OWM_MAX = "max";
+//        final String OWM_MIN = "min";
+//        final String OWM_DATETIME = "dt";
+//        final String OWM_DESCRIPTION = "main";
+//
+//        JSONObject forecastJson = new JSONObject(forecastJsonStr); //this one is not in the JSON data.
+//        JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST); //this is found in the list object of the JSON data.
+//
+//        String[] resultStrs = new String[numDays];
+//        for (int i = 0; i < weatherArray.length(); i++) {
+//            // For now, using the format "Day, description, hi/low"
+//            String day;
+//            String description;
+//            String highAndLow;
+//
+//            // Get the JSON object representing the day
+//            JSONObject dayForecast = weatherArray.getJSONObject(i);
+//
+//            // The date/time is returned as a long.  We need to convert that
+//            // into something human-readable, since most people won't read "1400356800" as
+//            // "this saturday".
+//            long dateTime = dayForecast.getLong(OWM_DATETIME);
+//            day = getReadableDateString(dateTime);
+//
+//            // description is in a child array called "weather", which is 1 element long.
+//            JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+//            description = weatherObject.getString(OWM_DESCRIPTION);
+//
+//            // Temperatures are in a child object called "temp".  Try not to name variables
+//            // "temp" when working with temperature.  It confuses everybody.
+//            JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+//            double high = temperatureObject.getDouble(OWM_MAX);
+//            double low = temperatureObject.getDouble(OWM_MIN);
+//
+//            // KO - I'd like a check for the setting that we're on, which would multiply
+//            // the Celsius temperature by 1.8 and add 32 if the mode were imperial.
+//            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+//
+//            boolean isImperial = prefs.getString("temperature",     //prefs.getString() has 2 parameters
+//                    mContext.getString(R.string.pref_temp_label)).equals("Imperial");
+//            if (isImperial) {
+//                high = convertToFahrenheit(high);
+//                low = convertToFahrenheit(low);
+//            }
+//
+//            highAndLow = formatHighLows(high, low);
+//            resultStrs[i] = day + " - " + description + " - " + highAndLow;
+//        }
+//
+//        return resultStrs;
+//    }
 
 
 
